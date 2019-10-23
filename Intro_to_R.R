b1cf3ad74d8a3af0c2d97e0ec201ac2cc51f9e8e
Joellen Stivala <jmstivala@mix.wvu.edu>
2019-10-23 09:34:27 -0400

    Initial Commit

---
+++ Intro_to_R.R
@@ -0,0 +1,15 @@
+2+2
+log(42/7.3)
+log(42) - log(7.3)
+3+2*2
+g<-5
+g
+g==0
+z<-(c(1,2,3,4,5,6,7,8,9,10))
+h<-(c(1:50))
+?seq
+A<-matrix(c(1,4,2,5,3,6),nrow=2,ncol=3) #column wise
+A
+# when using subscript for matrix specify row, column
+h
+z>=5
